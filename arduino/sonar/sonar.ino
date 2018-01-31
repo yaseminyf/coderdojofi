65da2f83adc07cffac393e9083423bb6462403bb
Yasemin Yalcinkaya <yaseminyf@users.noreply.github.com>
2018-01-31 15:39:57 +0100

    Update sonar.ino
    -Corrected the name of the sensor.
    -Corrected indentation.

--- arduino/sonar/sonar.ino
+++ arduino/sonar/sonar.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-//HC RS04 Sensore ultrasuoni
+//HC SR04 Sensore ultrasuoni
 int triggerPort = 7;
 int echoPort = 8;
 
@@ -18,34 +18,35 @@ void setup(){
 }
 
 void loop(){
- angle+=segno;
- if (angle >= 175) segno=-10;
- if (angle <= 5) segno=10;
+  angle+=segno;
+  if (angle >= 175) segno=-10;
+  if (angle <= 5) segno=10;
  
- myservo.write(angle);
+  myservo.write(angle);
+  
 //porta bassa l'uscita del trigger
-digitalWrite( triggerPort, LOW );
+  digitalWrite( triggerPort, LOW );
  
 //invia un impulso di 10microsec su trigger
-digitalWrite( triggerPort, HIGH );
-delayMicroseconds( 10 );
-digitalWrite( triggerPort, LOW );
+  digitalWrite( triggerPort, HIGH );
+  delayMicroseconds( 10 );
+  digitalWrite( triggerPort, LOW );
  
-long duration = pulseIn( echoPort, HIGH ); //microsec
+  long duration = pulseIn( echoPort, HIGH ); //microsec
 
-if( duration > 38000 ) distanza=0;
-else distanza = 0.034 * duration / 2;  //cm
+  if( duration > 38000 ) distanza=0;
+  else distanza = 0.034 * duration / 2;  //cm
 
-Serial.print ("Angolo: "); 
-Serial.print (angle);
-Serial.print( " durata: " );
-Serial.print( duration );
-Serial.print( " " );
-Serial.print( "distanza: " );
-Serial.print( distanza );
-Serial.println( " " );
+  Serial.print ("Angolo: "); 
+  Serial.print (angle);
+  Serial.print( " durata: " );
+  Serial.print( duration );
+  Serial.print( " " );
+  Serial.print( "distanza: " );
+  Serial.print( distanza );
+  Serial.println( " " );
  
 //aspetta 1.5 secondi
-delay( 1500 );
+  delay( 1500 );
 
 }
